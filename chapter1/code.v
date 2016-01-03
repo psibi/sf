c5d7a2a73895b3f5333a5a4f59821c5655f4f66e
Sibi Prabakaran <sibi@psibi.in>
2016-01-03 20:42:41 +0530

    Till negation definition.

---
+++ chapter1/code.v
@@ -0,0 +1,117 @@
+Inductive day : Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day
+  | saturday : day
+  | sunday : day.
+
+Definition next_weekday (d:day) : day :=
+    match d with
+      | monday => tuesday
+      | tuesday => wednesday
+      | wednesday => thursday
+      | thursday => friday
+      | friday => monday
+      | saturday => monday
+      | sunday => monday
+    end.
+
+Eval compute in (next_weekday friday).
+
+Eval compute in (next_weekday (next_weekday saturday)).
+
+Example test_next_weekday:
+  (next_weekday (next_weekday saturday)) = tuesday.
+Proof.
+  simpl.
+  reflexivity.
+Qed.
+
+Inductive bool : Type :=
+  | true : bool
+  | false : bool.
+
+Definition negb (b:bool) : bool :=
+  match b with
+    | true => false
+    | false => true
+  end.
+
+Definition andb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+    | true => b2
+    | false => false
+  end.
+
+Definition orb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+    | true => true
+    | false => b2
+  end.
+
+Example test_orb1: (orb true false) = true.
+Proof. reflexivity. Qed.
+Example test_orb2: (orb false false) = false.
+Proof. reflexivity. Qed.
+Example test_orb3: (orb false true) = true.
+Proof. reflexivity. Qed.
+Example test_orb4: (orb true true) = true.
+Proof. reflexivity. Qed.
+
+Check true.
+
+Check (negb true).
+
+Eval compute in (negb true).
+
+Check negb.
+
+Module Playground1.
+
+Inductive nat : Type :=
+  | O : nat
+  | S : nat -> nat.
+
+Check O.
+
+Definition pred (n:nat) : nat :=
+  match n with
+    | O => O
+    | S n' => n'
+  end.
+
+End Playground1.
+
+Definition minustwo (n : nat) : nat :=
+  match n with
+      | O => O
+      | S O => O
+      | S (S n') => n'
+  end.
+
+Check (S (S (S (S O)))).
+
+Eval compute in (minustwo 3).
+
+Eval compute in pred O.
+Eval compute in pred (S (S O)).
+
+Eval compute in (minustwo (S (S O))).
+Eval compute in (minustwo (S (S (S (S O))))).
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+    | O => true
+    | S O => false
+    | S (S n') => evenb n'
+  end.
+
+Definition oddb (n:nat) : bool := negb (evenb n).
+
+Example test_oddb1: (oddb (S O)) = true.
+Proof. reflexivity. Qed.
+Example test_oddb2: (oddb (S (S (S (S O))))) = false.
+Proof. reflexivity. Qed.
+
