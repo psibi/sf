864eb490e8b314099caf057480e59d39e3f7a6d7
Sibi Prabakaran <sibi@psibi.in>
2016-01-04 23:38:21 +0530

    Rewrite proof.

--- chapter1/code.v
+++ chapter1/code.v
@@ -207,4 +207,13 @@ Proof.
   reflexivity. 
 Qed.
 
-
+Theorem plus_id_example : forall n m: nat,
+                            n = m -> n + n = m + m.
+Proof.
+  intros n m.
+  intros H.
+  rewrite <- H.
+  reflexivity.
+Qed.
+  
+    
