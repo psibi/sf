0c4272cd2c1a2f13e657905344ca33510829d66b
Sibi Prabakaran <sibi@psibi.in>
2016-01-09 05:09:03 +0530

    mult_0_plus' is defined.

--- chapter2-induction/Induction.v
+++ chapter2-induction/Induction.v
@@ -39,3 +39,12 @@ Proof.
     reflexivity.
 Qed.
 
+Theorem mult_0_plus' : forall n m : nat,
+  (0 + n) * m = n * m.
+Proof.
+  intros n m.
+  assert (H: 0 + n = n).
+    Case "Proof of assertion". reflexivity.
+  rewrite -> H.
+  reflexivity.
+Qed.
