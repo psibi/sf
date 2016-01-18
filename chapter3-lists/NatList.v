dbb2177d2f6e1319c7cc20d5986322dd42351436
Sibi Prabakaran <sibi@psibi.in>
2016-01-19 02:28:23 +0530

    Till surjective_pairing_stuck.

--- chapter3-lists/NatList.v
+++ chapter3-lists/NatList.v
@@ -25,3 +25,36 @@ Notation "( x , y )" := (pair x y).
 Eval compute in (fst (3,5)).
 Eval compute in (snd (3,5)).
 
+Definition fst' (p: natprod) :=
+  match p with
+    | (x,y) => x
+  end.
+
+Definition snd' (p:natprod) :=
+  match p with
+    | (x,y) => y
+  end.
+
+Definition swap_pair (p: natprod) : natprod :=
+  match p with
+    | (x,y) => (y,x)
+  end.
+
+Theorem surjective_pairing' : forall (n m : nat),
+  (n,m) = (fst (n,m), snd (n,m)).
+Proof.
+  intros n m.
+  simpl.
+  reflexivity.
+Qed.
+
+Theorem surjective_pairing_stuck : forall (p : natprod),
+  p = (fst p, snd p).
+Proof.
+  intros p.
+  destruct p.
+  simpl.
+  reflexivity.
+Qed.
+      
+
