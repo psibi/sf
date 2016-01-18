4a36425e3c7e882cf4a31768b426161304c7fc56
Sibi Prabakaran <sibi@psibi.in>
2016-01-19 02:56:04 +0530

    Till app function. (++)

--- chapter3-lists/NatList.v
+++ chapter3-lists/NatList.v
@@ -56,5 +56,62 @@ Proof.
   simpl.
   reflexivity.
 Qed.
-      
 
+Theorem snd_fst_is_swap : forall (p : natprod),
+  (snd p, fst p) = swap_pair p.
+Proof.
+  intros p.
+  destruct p.
+  simpl.
+  reflexivity.
+Qed.
+
+Theorem fst_swap_is_snd : forall (p : natprod),
+  fst (swap_pair p) = snd p.
+Proof.
+  intros p.
+  destruct p.
+  reflexivity.
+Qed.
+
+Inductive natlist : Type :=
+  | nil : natlist
+  | cons : nat -> natlist -> natlist.
+
+Definition mylist := cons 1 (cons 2 (cons 3 nil)).
+
+Notation "x :: l" := (cons x l) (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
+Fixpoint repeat (n count : nat) : natlist := 
+  match count with
+    | O => nil
+    | (S n') => cons n (repeat n n')
+  end.
+
+Fixpoint length (l:natlist) : nat := 
+  match l with
+    | [] => O
+    | x :: xs => 1 + length xs
+  end.
+
+Check (length mylist).
+Eval compute in (length mylist).
+
+Fixpoint app (l1 l2 : natlist) : natlist := 
+  match l1 with
+      | nil => l2
+      | x :: xs => x :: (app xs l2)
+  end.
+
+Notation "x ++ y" := (app x y)
+                     (right associativity, at level 60).
+
+Example test_app1: [1;2;3] ++ [4;5] = [1;2;3;4;5].
+Proof. reflexivity. Qed.
+Example test_app2: nil ++ [4;5] = [4;5].
+Proof. reflexivity. Qed.
+Example test_app3: [1;2;3] ++ nil = [1;2;3].
+Proof. reflexivity. Qed.
+              
