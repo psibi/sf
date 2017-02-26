9bd3f0dc417993cc6e94e326b76320ce81db90c4
Sibi Prabakaran <sibi@psibi.in>
2017-02-26 23:40:32 +0530

    Finish andb3 related proofs

--- chapter1/Exercise.v
+++ chapter1/Exercise.v
@@ -21,3 +21,23 @@ Proof. reflexivity. Qed.
 
 Example test_nandb4: (nandb true true) = false.
 Proof. reflexivity. Qed.
+
+(* This function should return true when all of its inputs are true, and false otherwise. *)
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool := andb (andb b1 b2) b3.
+
+(* This doesn't work unless symbols are defined in that module. Why ? *)
+(* Definition andb3_symbol (b1:bool) (b2:bool) (b3:bool) : bool := b1 && b2 && b3. *)
+
+Example test_andb31: (andb3 true true true) = true.
+Proof. reflexivity. Qed.
+Example test_andb32: (andb3 false true true) = false.
+Proof. reflexivity. Qed.
+Example test_andb33: (andb3 true false true) = false.
+Proof. reflexivity. Qed.
+Example test_andb34: (andb3 true true false) = false.
+Proof. reflexivity. Qed.
+
+Check negb.
+  
+
