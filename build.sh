f2ab81b57f6debf4bb6bd658506f5a4564386ef2
Sibi Prabakaran <sibi@psibi.in>
2017-03-17 01:42:49 +0530

    Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-pushd $PWD
-cd chapter1
+mkdir obj
+pushd chapter1
 coqc Basics.v
+cp *.vo ../obj
 popd
+pushd chapter2-induction
+coqc Induction.v
+cp *.vo ../obj
+popd
+cp ./obj/*.vo ./chapter1/ 
+cp ./obj/*.vo ./chapter2-induction/
+cp ./obj/*.vo ./chapter3-lists/
