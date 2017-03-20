6f3a9fa596c33f3b51c038dbe6cbf9586eb2ba1e
Sibi Prabakaran <sibi@psibi.in>
2017-03-21 01:16:55 +0530

    Prove theorems

--- chapter4-poly/Poly.v
+++ chapter4-poly/Poly.v
@@ -123,6 +123,60 @@ Proof.
   - simpl. rewrite -> IHl'. reflexivity.
 Qed.
 
+Theorem app_assoc : forall A (l m n:list A),
+  l ++ m ++ n = (l ++ m) ++ n.
+Proof.
+  intros A l m n.
+  induction l.
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHl. reflexivity.
+Qed.
+
+Lemma app_length : forall (X:Type) (l1 l2 : list X),
+  length (l1 ++ l2) = length l1 + length l2.
+Proof.
+  intros X l1 l2.
+  induction l1.
+  - simpl. reflexivity.
+  - simpl. rewrite -> IHl1. reflexivity.
+Qed.
+
+Theorem rev_app_distr: forall X (l1 l2 : list X),
+  rev (l1 ++ l2) = rev l2 ++ rev l1.
+Proof.
+  intros X l1 l2.
+  induction l1.
+  - simpl. rewrite -> app_nil_r. reflexivity.
+  - simpl. rewrite -> IHl1. rewrite -> app_assoc.
+    reflexivity.
+Qed.
+
+Theorem rev_involutive_helper : forall X : Type, forall l : list X, forall x: X,
+      rev (l ++ [x]) = [x] ++ rev l.
+Proof.
+  intros X l x.
+  induction l.
+  - simpl. reflexivity.
+  - simpl.
+    rewrite -> IHl.
+    rewrite <- app_assoc at 1.
+    simpl. reflexivity.
+Qed.
+
+Theorem rev_involutive : forall X : Type, forall l : list X,
+  rev (rev l) = l.
+Proof.
+  intros X l.
+  induction l.
+  - simpl. reflexivity.
+  - simpl. 
+    rewrite -> rev_involutive_helper.
+    rewrite -> IHl at 1.
+    simpl.
+    reflexivity.
+Qed.
 
     
+  
+  
 
