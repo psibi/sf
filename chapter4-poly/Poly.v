f75cc5dd657b70dc1b75749c711dd1f32651e745
Sibi Prabakaran <sibi@psibi.in>
2017-03-21 02:55:01 +0530

    Add more proofs

--- chapter4-poly/Poly.v
+++ chapter4-poly/Poly.v
@@ -176,6 +176,58 @@ Proof.
     reflexivity.
 Qed.
 
+Inductive prod (X Y : Type) : Type :=
+| pair : X -> Y -> prod X Y.
+
+Arguments pair {X} {Y} _ _.
+
+Notation "( x , y )" := (pair x y).
+
+Notation "X * Y" := (prod X Y) : type_scope.
+
+Definition fst {X Y : Type} (p : X * Y) : X :=
+  match p with
+  | (x, y) => x
+  end.
+
+Definition snd {X Y : Type} (p : X * Y) : Y :=
+  match p with
+  | (x, y) => y
+  end.
+
+Fixpoint combine {X Y : Type} (lx : list X) (ly : list Y)
+           : list (X*Y) :=
+  match lx, ly with
+  | [], _ => []
+  | _, [] => []
+  | x :: tx, y :: ty => (x, y) :: (combine tx ty)
+  end.
+
+Check @combine.
+
+Compute (combine [1;2] [false;false;true;true]).
+
+Fixpoint split2 {X Y : Type} (l : list (X*Y)) (g :(list X) * (list Y))
+               : (list X) * (list Y) :=
+  match l with
+    | [] => g
+    | (x,y)::xs => split2 xs ((fst g) ++ [x],(snd g) ++ [y])
+  end.
+
+Fixpoint split {X Y : Type} (l : list (X*Y))
+               : (list X) * (list Y) :=
+split2 l ([],[]).
+
+Example test_split:
+  split [(1,false);(2,false)] = ([1;2],[false;false]).
+Proof.
+  simpl.
+  reflexivity.
+Qed.
+
+(* TODO: Find out where clause of coq and rewrite split function *)
+
+
     
   
   
