9379a1e3bf435b472a9a0cdb0d4c009aa2da7e81
Sibi Prabakaran <sibi@psibi.in>
2017-03-20 23:32:06 +0530

    Adapt build script for chapter 3

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ pushd chapter2-induction
 coqc Induction.v
 cp *.vo ../obj
 popd
+pushd chapter3-lists
+coqc NatList.v
+cp *.vo ../obj
+popd
 cp ./obj/*.vo ./chapter1/ 
 cp ./obj/*.vo ./chapter2-induction/
 cp ./obj/*.vo ./chapter3-lists/
+cp ./obj/*.vo ./chapter4-poly/
