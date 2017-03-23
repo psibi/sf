a0917b122d65e1735551db5a5f1da525645bc319
Sibi Prabakaran <sibi@psibi.in>
2017-03-23 06:12:29 +0530

    Start Chapter 5

---
+++ chapter5-tactics/Tactics.v
@@ -0,0 +1,59 @@
+Require Export Poly.
+
+Theorem silly1 : forall (n m o p : nat),
+     n = m ->
+     [n;o] = [n;p] ->
+     [n;o] = [m;p].
+Proof.
+  intros n m o p eq1 eq2.
+  rewrite <- eq1.
+  apply eq2.
+Qed.
+
+Theorem silly2 : forall (n m o p : nat),
+     n = m ->
+     (forall (q r : nat), q = r -> [q;o] = [r;p]) ->
+     [n;o] = [m;p].
+Proof.
+  intros n m o p.
+  intros eq1.
+  intros eq2.
+  apply eq2.
+  apply eq1.
+Qed.
+
+Theorem silly2a : forall (n m : nat),
+     (n,n) = (m,m) ->
+     (forall (q r : nat), (q,q) = (r,r) -> [q] = [r]) ->
+     [n] = [m].
+Proof.
+  intros n m.
+  intros eq1.
+  intros eq2.
+  apply eq2.
+  apply eq1.
+Qed.
+
+Theorem silly_ex :
+     (forall n, evenb n = true -> oddb (S n) = true) ->
+     evenb 3 = true ->
+     oddb 4 = true.
+Proof.
+  intros H1.
+  intros H2.
+  apply H1.
+  apply H2.
+Qed.
+
+Lemma rev_empty : forall (X : Type) (l : list X),
+    l = [] -> rev l = [].
+Proof.
+  intros X l.
+  intros H.
+  rewrite -> H.
+  simpl. reflexivity.
+Qed.
+
+
+  
+
