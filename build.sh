d8541e0c12c8745bdbe46353280aff8c4d43dc24
Sibi Prabakaran <sibi@psibi.in>
2017-04-02 06:08:04 +0530

    Update build script

--- build.sh
+++ build.sh
@@ -17,6 +17,10 @@ cp *.vo ../obj
 popd
 pushd chapter5-tactics
 popd
+pushd chapter6-logic
+coqc Logic.v
+cp *.vo ../obj
+popd
 cp ./obj/*.vo ./chapter1/ 
 cp ./obj/*.vo ./chapter2-induction/
 cp ./obj/*.vo ./chapter3-lists/
